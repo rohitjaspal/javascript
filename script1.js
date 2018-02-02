81ce5ec564605bc4ee6e71cf2c6f3b105989837a
Rohit Jaspal <jaspalrohitpanchkula@gmail.com>
2018-02-02 22:47:18 +0530

    javascript

---
+++ script1.js
@@ -0,0 +1,52 @@
+ function fire()
+//var clickmebutton = document.getElementById('clickme');
+//clickmebutton.onclick = runtheexample;
+//document.getElementById('clickme').onclick = runTheExample;
+
+
+//}
+
+/*
+function runTheExample(){
+	alert('running the example');
+}
+*/
+/*
+function runTheExample(){
+var meElement = document.getElementById('second').nodeName;
+//var myNodename = myElement.Nodename;
+//var Nodename = meElement.nodeName;
+
+
+alert(meElement);
+*/
+{
+var anchor = document.getElementById('myAnchor');
+//var anchordestination = anchor.href;
+//alert(anchordestination);
+//anchor.href="http://www.google.com";
+anchor.setAttribute('href','http://www.facebook.com');
+anchor.getAttribute('href');			
+}
+/*
+
+
+if(meElement != null)
+{
+alert(meElement.parentNode.Nodename);
+}
+
+*/
+
+
+
+
+
+
+
+/*
+function runTheExample(){
+	alert('running the example');
+}*/
+
+
